f289a0272425e0b3f362ba450c1c1f2e92a15368
InsaneSkillz27 <nhussey27@gmail.com>
2018-06-24 10:58:09 -0400

    sadas

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ bot.on("message", async message => {
 
     if(cmd === `${prefix}application`){
         let sembed = new Discord.RichEmbed()
-        .addField(":white_check_mark: `A private message with more information has been sent to you`")
+        .addField(":white_check_mark: A private message with more information has been sent to you")
         .setColor(colors.green)
 
         return message.channel.send(sembed)
