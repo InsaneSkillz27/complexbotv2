c89f3f2a430ac03c55ee4d2a7044bfd1f59d45b3
InsaneSkillz27 <nhussey27@gmail.com>
2018-06-24 11:49:01 -0400

    7 b gbnhf

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ bot.on("message", async message => {
 	if (cmd === `${prefix}application`) {
 
 		let sembed = new Discord.RichEmbed()
+            .setColor(colors)
             .addField(":white_check_mark: A private message with more information has been sent to you")
-			.setColor(colors.green)
             .setFooter("application template")
 		let aembed = new Discord.RichEmbed()
 			.setAuthor(message.author.id)
