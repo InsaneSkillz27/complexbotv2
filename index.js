9bf81d5c4ddc93787a2e3d4a10fd73ad1d349447
InsaneSkillz27 <nhussey27@gmail.com>
2018-06-26 17:33:41 -0400

    fix 2

--- index.js
+++ index.js
@@ -20,20 +20,20 @@ bot.on("guildMemberRemove", async member =>{
 })
 
 bot.on("guildMemberAdd", async member => {
-    console.log(`${member} Joined The Server!`)
+    console.log(`${member.if} Joined The Server!`)
 
 
     let welcomechannel = member.guild.channels.find(`name`, "welcome_leave");    
 let jrandom = Math.floor(Math.random()* 5) + 1;
 //creating a custom and random join message
 if(jrandom === 1){
-    welcomechannel.send(`Looks like ***${member.username}*** has joined the server!`)
+    welcomechannel.send(`Looks like ***${member}*** has joined the server!`)
 }else if(jrandom === 2){
-    welcomechannel.send(`OH NO. Looks like ***${member.username}*** has joined the server!`)
+    welcomechannel.send(`OH NO. Looks like ***${member}*** has joined the server!`)
 }else if(jrandom === 3){
-    welcomechannel.send(`No need to fear, for ***${member.username}*** is here!`)
+    welcomechannel.send(`No need to fear, for ***${member}*** is here!`)
 }else if(jrandom === 4){
-    welcomechannel.send(`Everyone hide! ***${member.username}*** is here!!`)
+    welcomechannel.send(`Everyone hide! ***${member}*** is here!!`)
 }else{
     welcomechannel.send(`${member.username} HAS ARRIVED TO THE PARTY!`)
 }
