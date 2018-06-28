1ee52612144b6d5bfc6638cbb7e54a2ed9cae316
InsaneSkillz27 <nhussey27@gmail.com>
2018-06-28 14:36:33 -0400

    setprefix cmd

--- index.js
+++ index.js
@@ -72,12 +72,33 @@ bot.on("message", async message => {
 
     let prefix = prefixes[message.guild.id].prefixes;
     console.log(prefix)
-    
+
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
 
     if(cmd === `${prefix}setprefix`){
+        //!setprefix <symbol>
+
+        if(!message.member.hasPermission("MANAGE_SERVER")) return message.reply("🚫***You can not do that.***");
+        if(!args[0] || args[0 == "helpme"]) return message.reply("Usage: !prefix <prefered prefix> *Only one is reccomended*");
+
+        let prefixes = JSON.parse(fs.readFileSync("./prefix.json", "utf8"));
+
+        prefixes[message.guild.id] = {
+            prefixes: args[0]
+        };
+
+        fs.writeFile("./prefix.json", JSON.stringify(prefixes), (err) =>{
+            if(err) console.log(err)
+        })
+
+        let prefixEmbed = new Discord.RichEmbed()
+        .setColor(colors.green)
+        .setTitle("Prefix Set!")
+        .setDescription(`Set to ${args[0]}`);
+
+        message.channel.send(prefixEmbed);
 
     }
 
