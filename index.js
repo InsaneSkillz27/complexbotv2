75e541ee87fb0f6d68570891af9101539f00ccdf
InsaneSkillz27 <nhussey27@gmail.com>
2018-07-05 21:27:47 -0400

    warn add

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const bot = new Discord.Client({disableEveryone: true});
 
 let coins = require("./coins.json")
 let xp = require("./xp.json")
+let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"))
+
 
 bot.on("guildMemberRemove", async member =>{
     console.log(`${member.id} left the server!`)
@@ -77,7 +79,61 @@ bot.on("message", async message => {
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
 
-    if(cmd === `${prefix}setprefix`){
+    if(cmd === `${prefix}warn`){
+
+        //warn <@user#id Reason>
+        if(!message.member.hasPermission(MANAGE_MEMBERS)) return message.reply("***You maynot warn this user***");
+        let wUser = message.guild.member(message.mentions.user.first()) || message.members.get(args[0])
+        if(!wUser) return message.reply("***You must provide a user.***")
+        if(wUser.hasPermission("MANAGE_MESSAGES")) return message.reply("***You may not warn this user***")
+        let wReason = args.join(" ").slice(22);
+
+        if(!warns[wUser.id]) warns[wuser.id] = {
+            warns: 0
+        };
+
+        warns[wUser.id], warns++;
+
+        fs.writeFile("./warnings.json", JSON.stringify(warns), err => {
+           if (err) console.log(err);
+        });
+
+        let warnEmbed = new Discord.RichEmbed()
+        .setDescription("Warns")
+        .setAuthor(message.author.username)
+        .setColor(colors.red)
+        .addField("Warned User", `<@{wUser.id}>`)
+        .addField("Warned In", message.channel)
+        .addField("Number of Warnings", warns[wUser.id].warns)
+        .addField("Reason", wReason);
+
+        let warnchannel = message.guild.channels.find(`name`, "incidents");
+        if(!warnchannel) return message.reply("***Must create an 'incidents' channel.***");
+
+        warnchannel.send(warnEmbed);
+
+        if(warns[wUser.id].warns == 2){
+            let muterole = message.guild.roles.find(`name`, "[~] Muted");
+            if(!muterole) return message.reply("***You must create the '[~] Muted' role***");
+
+            let wMuteTime = "2d";
+            await(wUser.addRole(muterole.id));
+            message.channel.send(`${wUser.tag} has been muted for 2 days.`)
+
+            setTimeout(function(){
+                wUser.removeRole(muterole.id)
+                message.channel.reply(`${wUser.tag} has been unmuted.`)
+            })
+        }
+        if(warns[wUser.id].warns == 4){
+            message.guild.member(wUser).kick(reason)
+            message.channel.send(`${wUser.tag} Has Been Kicked.`)
+        };
+
+
+    }
+
+    if(cmd === "!setprefix"){
         //!setprefix <symbol>
 
         if(!message.member.hasPermission("MANAGE_SERVER")) return message.reply("🚫***You can not do that.***");
